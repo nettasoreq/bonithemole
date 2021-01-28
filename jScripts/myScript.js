129d53efdcc081697a79324a8c919b9503572644
nettasoreq <67923174+nettasoreq@users.noreply.github.com>
2021-01-28 13:00:08 +0200

    Add files via upload

---
+++ jScripts/myScript.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $(".about").click(function () {
+        $("#howToPlayDiv").hide();
+        $("#aboutDiv").toggle();
+    });
+
+    $(".howToPlay").click(function () {
+        $("#aboutDiv").hide();
+        $("#howToPlayDiv").toggle();
+    });
+
+    $(".closeAbout").click(function () {
+        $("#aboutDiv").hide();
+        $("#gameIframe")[0].contentWindow.focus();
+    });
+
+    $(".closeHowToPlay").click(function () {
+        $("#howToPlayDiv").hide();
+        $("#gameIframe")[0].contentWindow.focus();
+    });
+
+
+});
+
+
+
